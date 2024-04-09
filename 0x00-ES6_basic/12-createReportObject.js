505704a91a1195e91d16aae7ba21c67128fc04e7
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-09 18:31:36 +0000

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    employees: employeesList,
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    allEmployees: employeesList,
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
     },
   };
 }
