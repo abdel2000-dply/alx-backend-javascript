d6aaadd903729ba26e16e0917ace7a181a1d94aa
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-11 23:40:30 +0000

    Implement a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    // You cannot call a static method on an object, only on an object class.
+    return amount * conversionRate;
+  }
+}
