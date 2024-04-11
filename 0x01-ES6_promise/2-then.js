8ff654100336a917a003d9363c5f9be995a9690b
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-11 01:57:31 +0000

    Fix string quotes in 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,8 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
-      body: "success",
+      body: 'success',
     }))
     .catch(() => new Error())
-    .finally(() => console.log("Got a response from the API"));
+    .finally(() => console.log('Got a response from the API'));
 }
