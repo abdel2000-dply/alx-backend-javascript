36ba7fc10b1859f01e3572749f5e573867364c48
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-11 23:22:29 +0000

    Implement a class named Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+  set code(code) {
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
