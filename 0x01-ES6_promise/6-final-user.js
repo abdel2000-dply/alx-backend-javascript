c028a198f6231d55847f0ae1df3176d5823206a9
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-11 03:30:37 +0000

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    results.map((result) => {
+      if (result.status === 'rejected') {
+        return { status: 'rejected', value: result.value };
+      }
+      return { status: 'fulfilled', value: result.value };
+    });
+  });
+}
