880a10575966636d3db59492724119fa3881f7ef
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-11 19:52:40 +0000

    Refactor HolbertonCourse class to include type checking in constructor and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,37 +1,53 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name === 'string') this._name = name;
+    else throw new TypeError('Name must be a string');
+
+    if (typeof length === 'number') this._length = length;
+    else throw new TypeError('Length must be a number');
+
+    if (
+      Array.isArray(students)
+      && students.every((student) => typeof student === 'string')
+    ) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 
+  // getters
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
 
+  // setters
   set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
+    if (typeof newName === 'string') this._name = newName;
+    else throw new TypeError('Name must be a string');
   }
 
   set length(newLength) {
-    if (typeof newLength === 'number'){
-      this._length = newLength
-    } else {
-      throw new TypeError('Length must be a number');
-    }
+    if (typeof newLength === 'number') this._length = newLength;
+    else throw new TypeError('Length must be a number');
   }
 
   set students(newStudents) {
+    if (
+      Array.isArray(newStudents)
+      && newStudents.every((student) => typeof student === 'string')
+    ) {
       this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 }
