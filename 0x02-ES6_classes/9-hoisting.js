b666851a1f035fadc3734c65d2c907c416eac3a3
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-12 02:37:55 +0000

    fixing a typo

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -30,7 +30,7 @@ export class StudentHolberton {
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-  }
+  }  
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
@@ -42,4 +42,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
