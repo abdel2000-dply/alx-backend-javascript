1a1efbb3b1abb97ed6b063a24e048a81f1cc2f48
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-16 20:10:27 +0100

    fixing a bug

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,8 @@
 export const weakMap = new WeakMap();
-
 export function queryAPI(endpoint) {
-  let counter = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, counter + 1);
-
+  let counter = (weakMap.get(endpoint) || 0) + 1;
   if (counter >= 5) {
     throw new Error('Endpoint load is high');
   }
+  weakMap.set(endpoint, counter);
 }
