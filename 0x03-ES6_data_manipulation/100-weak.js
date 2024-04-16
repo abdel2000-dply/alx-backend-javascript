da2d7467f00fe117b0f2c3402739edb109dee387
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-16 20:14:36 +0100

    Refactor queryAPI function to use a concise if statement

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,6 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  let counter = (weakMap.get(endpoint) || 0) + 1;
-  if (counter >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+  const counter = (weakMap.get(endpoint) || 0) + 1;
+  if (counter >= 5) throw new Error('Endpoint load is high');
   weakMap.set(endpoint, counter);
 }
