4305c1857e9d85647e9fa9f5b8d1497ab871948b
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-16 03:02:48 +0100

    Refactor cleanSet function to handle empty startString parameter and filter non-string items

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
+  if (!startString || !set || typeof startString !== 'string') {
     return '';
   }
 
