1fbf3b553275ffe04cc0e12b7ba33c7aad50849e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-16 02:57:15 +0100

    Fix issue with cleanSet function not handling empty startString parameter

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set.size === 0 || !(set instanceof Set) || typeof startString !== 'string') {
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
 
