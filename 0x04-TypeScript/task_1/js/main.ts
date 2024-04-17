5e2a711e8e4c20b5230fc125aa73a22cd4243656
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-18 00:30:02 +0100

    Add TypeScript implementation of StudentClass

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,12 +21,37 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17
-};
+// Write a Class named StudentClass:
 
-console.log(director1);
+// The constructor accepts firstName(string) and lastName(string) arguments
+// The class has a method named workOnHomework that return the string Currently working
+// The class has a method named displayName. It returns the firstName of the student
+// The constructor of the class should be described through an Interface
+// The class should be described through an Interface
+// Requirements:
+
+// You can reuse the Webpack configuration from the previous exercise to compile the code.
+// When running npm run build, no TypeScript error should be displayed.
+// Every variable should use TypeScript when possible.
+
+interface Student {
+  firstName: string;
+  lastName: string;
+}
+
+class StudentClass implements Student {
+  constructor(readonly firstName: string, readonly lastName: string) {}
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+const student1 = new StudentClass('Alice', 'Smith');
+
+console.log(student1.workOnHomework()); // working
+console.log(student1.displayName()); // working!
