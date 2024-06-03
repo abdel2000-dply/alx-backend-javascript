e9ca60b3c9027e57fc5ab6c4d332905cffcb12a5
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-03 16:53:38 +0100

    Refactor stdin event listener to use 'readable' event

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 // Using Process stdin and stdout
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) {
+    const name = data.toString().trim();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}\n`);
+    }
   }
 });
 
