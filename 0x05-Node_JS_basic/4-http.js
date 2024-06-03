520211a73a46c1a8ad955be1ec3fb4c62fb0e19f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-03 17:26:37 +0100

    Fix formatting issue and refactor HTTP server code

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,9 +4,8 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!')
-}
-);
+  res.end('Hello Holberton School!');
+});
 
 app.listen(port);
 
