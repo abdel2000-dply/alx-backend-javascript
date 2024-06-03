0379e507ed5e7b38b6485d5a86562743586808b3
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-03 15:26:53 +0100

    Add synchronous file reading function

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+// Reading a file synchronously with Node JS
+const fs = require('fs');
+
+function countStudents(path){
+  let data;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  }
+  catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  const lines = data.split('\n').filter((line) => line);
+  console.log(`Number of students: ${lines.length - 1}`);
+  const students = lines.slice(1).map(line => line.split(','))
+
+  const fields = {};
+  for (const student of students) {
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(student[0]);
+  }
+
+  for (const field in fields) {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
