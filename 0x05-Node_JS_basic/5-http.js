44246d2cb28387cc3f27483489146fd27ca97fb8
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-03 18:24:52 +0100

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,18 +3,30 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(Error('Cannot load the database'));
-      } else {
-        const content = data.toString().split('\n');
-        const students = content.filter((item) => item);
-        students.shift();
-        const numOfStudents = students.length;
-        const csStudents = students.filter((item) => item.includes('CS')).length;
-        const sweStudents = students.filter((item) => item.includes('SWE')).length;
-        resolve(`Number of students: ${numOfStudents}\nNumber of students in CS: ${csStudents}\nNumber of students in SWE: ${sweStudents}`);
+        return;
       }
+
+      const result = []
+      const lines = data.split('\n').filter((line) => line);
+      result.push(`Number of students: ${lines.length - 1}`);
+      const students = lines.slice(1).map((line) => line.split(','));
+
+      const fields = {};
+      students.forEach((student) => {
+        if (!fields[student[3]]) fields[student[3]] = [];
+        fields[student[3]].push(student[0]);
+      });
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          result.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+
+      resolve(result.join('\n'));
     });
   });
 }
