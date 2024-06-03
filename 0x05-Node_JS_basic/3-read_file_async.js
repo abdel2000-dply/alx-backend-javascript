d05cce849aea17fecb0ccf1f7c8fa7a53217316f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-03 16:41:27 +0100

    Add asynchronous file reading function

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+// reading file asynchronously
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+
+      const lines = data.split('\n').filter((line) => line);
+      console.log(`Number of students: ${lines.length - 1}`);
+      const students = lines.slice(1).map((line) => line.split(','));
+
+      const fields = {};
+      students.forEach((student) => {
+        if (!fields[student[3]]) fields[student[3]] = [];
+        fields[student[3]].push(student[0]);
+      });
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
