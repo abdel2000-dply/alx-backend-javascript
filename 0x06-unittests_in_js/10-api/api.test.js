d23afcb879450ec9e202a008abf318a2badcc5a4
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-05 23:38:55 +0100

    Add unit tests for API endpoints

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,57 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index page', () => {
+  it('should return status 200', (done) => {
+    request.get('http://localhost:7865', (err, res) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct body', (done) => {
+    request.get('http://localhost:7865', (err, res) => {
+      expect(res.body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return status 200 with the correct body', (done) => {
+    request.get('http://localhost:7865/cart/12', (err, res) => {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status 404 when the ID is not a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
+describe('GET /available_payments', () => {
+  it('should return status 200 with the correct body', (done) => {
+    request.get('http://localhost:7865/available_payments', (err, res) => {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      done();
+    });
+  });
+});
+
+describe('POST /login', () => {
+  it('should return "Welcome Abdellah"', (done) => {
+    request.post({
+      url: 'http://localhost:7865/login',
+      form: { userName: 'Abdellah' }
+    }, (err, res) => {
+      expect(res.body).to.equal('Welcome Abdellah');
+      done();
+    });
+  });
+});
