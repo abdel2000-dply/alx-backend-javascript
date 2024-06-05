5e8525fb1edca3a13413c6d3ac762f4c5713c8f2
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-05 23:03:47 +0100

    Add API endpoint for payment system

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
