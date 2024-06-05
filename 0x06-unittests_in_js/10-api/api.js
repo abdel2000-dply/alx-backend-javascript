4383dd77d9023d9d089c32bf7019949313a90145
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-05 23:43:46 +0100

    Add express.json middleware for parsing JSON data

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
