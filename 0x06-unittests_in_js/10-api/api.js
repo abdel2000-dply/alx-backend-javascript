fa30ad8bd7ddae74f01266d19ec225f8526f413f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-06-05 23:38:45 +0100

    Add API endpoint for login functionality

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  res.send(`Welcome ${req.body.userName}`)
+})
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
